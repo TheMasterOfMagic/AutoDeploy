52868dbae41d9abb6a50bff4f130832f5226d347
TheMasterOfMagic <18811569079@163.com>
2019-07-11 17:07:11 +0800

    fix bugs in utils.sh

--- utils.sh
+++ utils.sh
@@ -9,7 +9,7 @@ check_rc () {
 
 update_apt_if_needed () {
     start "update apt if needed"
-    last_update=$(stat -c %Y /var/cache/apt/pkgcache.bin 2> /dev/null || true) 
+    last_update=$(stat -c %Y /var/cache/apt/pkgcache.bin 2> /dev/null || echo 0) 
     now=$(date +%s)
     if [[ $((now - last_update)) -gt 3600 ]]; then
         log "updating apt"
