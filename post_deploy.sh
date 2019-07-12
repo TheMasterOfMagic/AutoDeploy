c67656a4c2165b24befb7b41dacd3fac4cbeede4
TheMasterOfMagic <18811569079@163.com>
2019-07-12 11:25:51 +0800

    refactor some codes

--- post_deploy.sh
+++ post_deploy.sh
@@ -8,17 +8,16 @@ set_env_var () {
     value="${2}"
     rc_file="${3:-${HOME}/.zshrc}"
     start "setting environment variables ${key}=${value}"
-    if grep "^export ${key}=" "${rc_file}" &> /dev/null; then
-        sed -i "s#^export ${key}=.*#export ${key}=${value}#g" "${rc_file}"
-    else
-        printf "\nexport ${key}=${value}\n" >> "${rc_file}"
+    script="export ${key}=${value}"
+    if ! grep "^${script}$" "${rc_file}" &> /dev/null; then
+        printf "\n${script}" >> "${rc_file}"
     fi
     end $?
 }
 
 post_deploy () {
     start "post deploying"
-    set_env_var "LANG" "en_US.UTF-8"
+    set_env_var "LANG" "en_US.UTF-8" && \
     set_env_var "PATH" "\${PATH}:\${HOME}/anaconda3/bin"
     end $?
     log "Note: You may need to start a new terminal to apply some changes."
