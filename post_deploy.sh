8dd91fd7dd7210e5949d3cf02ea022637a88819c
TheMasterOfMagic <18811569079@163.com>
2019-07-12 11:14:10 +0800

    refactor some codes

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,18 +1,25 @@
 . ./utils.sh
 
-configure_locale () {
-    start "configuring locale"
-    if grep "^export LANG=" "${HOME}/.zshrc" &> /dev/null; then
-        sed -i "s/^export LANG=.*/export LANG=en_US.UTF-8/g" "${HOME}/.zshrc"
+set_env_var () {
+    if [[ -z "${2:-}" ]]; then
+        return 1
+    fi
+    key="${1}"
+    value="${2}"
+    rc_file="${3:-${HOME}/.zshrc}"
+    start "setting environment variables ${key}=${value}"
+    if grep "^export ${key}=" "${rc_file}" &> /dev/null; then
+        sed -i "s#^export ${key}=.*#export ${key}=${value}#g" "${rc_file}"
     else
-        printf "\nexport LANG=en_US.UTF-8\n" >> "${HOME}/.zshrc"
+        printf "\nexport ${key}=${value}\n" >> "${rc_file}"
     fi
     end $?
 }
 
 post_deploy () {
     start "post deploying"
-    configure_locale
+    set_env_var "LANG" "en_US.UTF-8"
+    set_env_var "PATH" "\${PATH}:\${HOME}/anaconda3/bin"
     end $?
     log "Note: You may need to start a new terminal to apply some changes."
 }
